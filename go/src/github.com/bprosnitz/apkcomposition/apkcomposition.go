951abebaa87c9597d24255efe8afed84230314bb
Benj Prosnitz <bprosnitz@bprosnitz-goo.sin.corp.google.com>
2016-07-27 11:56:57 +0800

    print filenames

--- go/src/github.com/bprosnitz/apkcomposition/apkcomposition.go
+++ go/src/github.com/bprosnitz/apkcomposition/apkcomposition.go
@@ -1,4 +1,30 @@
-package apkcomposition
+package main
 
 import (
+  "flag"
+  "fmt"
+  "log"
+  "archive/zip"
 )
+
+func main() {
+  flag.Parse()
+
+  if flag.NArg() != 1 {
+    log.Fatalf("Please specify apk")
+  }
+
+  r, err := zip.OpenReader(flag.Arg(0))
+  if err != nil {
+    log.Fatal(err)
+  }
+  processApk(r)
+  r.Close()
+}
+
+func processApk(r *zip.ReadCloser) {
+
+  for _, f := range r.File {
+    fmt.Printf("Contents of %s:\n", f.Name)
+  }
+}
